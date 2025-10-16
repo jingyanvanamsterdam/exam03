cbae08429104bd4c83f6030bd462ec9527b2eac4
jingyan <jingyan.dong420@gmail.com>
2025-10-16 18:43:44 +0200

    finished powerset.

--- powerset/powerset.c
+++ powerset/powerset.c
@@ -5,17 +5,17 @@ typedef struct	s_set
 {
 	int	*set;
 	int	count;
-	int	sum;
+	int	num;
 }				t_set;
 
-void	print_set(t_set *set)
+void	print_set(int *arr, int count)
 {
 	int	 i = 0;
-
-	while (i < set->count)
+	printf("set is: ");
+	while (i <= count)
 	{
-		printf("%d", set->set[i]);
-		if (i == set->count - 1)
+		printf("%d", arr[i]);
+		if (i == count)
 			printf("\n");
 		else
 			printf(" ");
@@ -23,93 +23,50 @@ void	print_set(t_set *set)
 	}
 }
 
-void reset_set(t_set *set)
-{
-	if (set->set)
-		free(set->set);
-	set->set = NULL;
-	set->count = 0;
-	set->sum = 0;
-}
-
-int	powerset(int *arr, int elements, int num, t_set *set)
+int	calculate_sum(int *arr, int count)
 {
 	int	i = 0;
-
-	while (i < elements) // elements - 1 or elements
+	int	sum = 0;
+	while (i <= count)
 	{
-		printf("i = %d\n", i);
-		printf("set: ");
-		for (int j = 0; j < set->count; j++)
-		{
-			printf("%d ", set->set[j]);
-		}
-		printf("\n");
-		printf("elements = %d\n", elements);
-		set->sum += arr[i];
-		set->count ++;
-		if (set->sum <= num)
-		{
-			set->set = realloc(set->set, sizeof(int) *(set->count));
-			if (!set->set)
-				return (reset_set(set), 1);
-			set->set[set->count - 1] = arr[i];
-			if (set->sum == num)
-			{
-				print_set(set);
-				reset_set(set);
-				i++;
-			}
-			else if (powerset(&arr[i + 1], (elements - 1 - i), num, set) == 0)
-			{
-				set->sum -= arr[i];
-				set->count --;
-				i++;
-			}	
-		}
-		else
-		{
-			set->sum -= arr[i];
-			set->count --;
-			i++;
-		}
+		sum += arr[i];
+		i++;
 	}
-	return (0);
+	return (sum);
+}
+
+void	powerset(t_set *set, int i, int j, int *arr)
+{
+	if (i >= set->count)
+		return ;
+	arr[j] = set->set[i];
+	if (calculate_sum(arr, j) == set->num)
+		print_set(arr, j);
+	powerset(set, i + 1, j + 1, arr);
+	arr[j] = 0;
+	powerset(set, i + 1, j, arr);
+	return ;
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc < 3)
 		return (1);
-	
-	int	num = atoi(argv[1]);
 	int	*arr = calloc(argc - 2, sizeof(int));
 	if (!arr)
 		return (1);
+	t_set	set;
+	set.set = calloc(argc - 2, sizeof(int));
+	if (!set.set)
+		return 1;
 	int i;
 	for (i = 0; i < argc - 2; i++)
-	{
-		arr[i] = atoi(argv[i + 2]);
-	}
-	t_set	*set;
-	set = malloc(sizeof(t_set));
-	if (!set)
-		return (1);
-	set->count = 0;
-	set->set = NULL;
-	set->sum = 0;
-	int	j = 0;
-	while (powerset(&arr[j + 1], i - 1 - j, num, set) == 0 && j < i)
-	{
-		reset_set(set);
-		j++;
-	}
-	if (j == i)
-	{
-		reset_set(set);
-		free(arr);
-		return 0;
-	}
+		set.set[i] = atoi(argv[i + 2]);
+	set.num = atoi(argv[1]);
+	set.count = i;
+	powerset(&set, 0, 0, arr);
+	if (set.set)
+		free(set.set);
 	free(arr);
 	return (0);
 }
