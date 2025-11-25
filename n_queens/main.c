91dce37b4ac00afe073a816a7d69fb26ded8a806
jingyan <jingyan.dong420@gmail.com>
2025-11-25 23:05:27 +0100

    update

--- n_queens/main.c
+++ n_queens/main.c
@@ -23,52 +23,54 @@ void	handle_solution(int l, int grid[l][l])
 
 int	check_grid(int l, int grid[l][l], int i, int j)
 {
-	int	x;
-	int	y;
-
-	for (x = 0; x <= i; x++)
+	if (j == 0)
 	{
-		for (y = 0; y < j; y++)
+		if (i == 0)
+			return (1);
+		else
 		{
-			if (grid[x][y] == 1)
+			while (i >= 0)
 			{
-				if (x == i )
+				if (grid[i - 1][j] == 1)
+					return (0);
+				else
+					i--;
 			}
+			return (1);
 		}
 	}
+	else
+	{
+		
+	}
+
 }
 /**
  * return 1 is solved,
  * return 0 is not.
  */
-int solve_queens(int l, int grid[l][l], int i, int j)
+int solve_queens(int l, int grid[l][l], int i)
 {
-	if (i == l && j == l)
+	int	j = 0;
+	if (i == l)
 		return (0);
-	while (i < l)
+	while (j < l)
 	{
-		while (j < l)
+		grid[i][j] = 1;
+		if (check_grid(l, grid, i, j))
 		{
-			grid[i][j] = 1;
-			if (check_grid(l, grid, i, j))
-			{
-				j++;
-				continue;
-			}
-			else
-			{
-				grid[i][j] = 0;
-				if (j + 1 == l)
-				{
-					if (solve_queens(l, grid, i - 1, j))
-						handle_solution(l, grid);
-				}
-				grid[i][j] = 0;
-				j++;
-			}
+			if (i == l - 1)
+				return (1);
+			else if (solve_queens(l, grid, i + 1))
+				return (1);
 		}
-		i++;
+		else
+		{
+			grid[i][j] = 0;
+			j++;
+		}
 	}
+	return (0);
 }
 
 void print_grid(int l, int grid[l][l])
@@ -102,11 +104,12 @@ int	main(int argc, char **argv)
 	int	grid[l][l];
 	int res;
 	int	x = 0;
-	int y = 0;
 
 	init_grid(l, grid);
-	res = solve_queens(l, grid, x, y);
+	res = solve_queens(l, grid, x);
 	if (!res)
 		return (write(1, "\n", 1));
+	else
+		handle_solution(l, grid);
 	return (0);
 }
